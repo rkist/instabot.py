63856c1139c29ceef0cb13b5ecfe91e4ff8c00af
rkist <raulkist@gmail.com>
2020-09-16 23:01:22 -0300

    add consumista stuff

--- consumista.bot.py
+++ consumista.bot.py
@@ -9,49 +9,54 @@ from src.feed_scanner import feed_scanner
 from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
+def ReadFileData(filePath):  
+    with open(filePath) as f:
+        content = f.readlines()
+    data = [x.strip() for x in content]
+    return data
+
+def ReadBotConf(confFolder):
+    confs = {}
+    confs["loginData"] = ReadFileData(os.path.join(confFolder, "login.botconf"))
+    confs["unwantedUsernameList"] = ReadFileData(os.path.join(confFolder, "unwanted_username_list.botconf"))
+    confs["unfollowWhitelist"] = ReadFileData(os.path.join(confFolder, "unfollow_whitelist.botconf"))
+    confs["tagList"] = ReadFileData(os.path.join(confFolder, "tag_list.botconf"))
+    confs["tagBlacklist"] = ReadFileData(os.path.join(confFolder, "tag_blacklist.botconf"))
+    return confs
+
+confs = ReadBotConf("consumista")
+
+
 bot = InstaBot(
-    login="username",
-    password="password",
-    like_per_day=1000,
+    login=confs["loginData"][0],
+    password=confs["loginData"][1],
+    like_per_day=500,
     comments_per_day=0,
-    tag_list=['follow4follow', 'f4f', 'cute'],
-    tag_blacklist=['rain', 'thunderstorm'],
-    user_blacklist={},
-    max_like_for_one_tag=50,
-    follow_per_day=300,
-    follow_time=1 * 60,
-    unfollow_per_day=300,
-    unfollow_break_min=15,
-    unfollow_break_max=30,
-    log_mod=0,
-    proxy='',
+    tag_list = confs["tagList"],
+    tag_blacklist = confs["tagBlacklist"],
+    user_blacklist = {},
+    max_like_for_one_tag = 30,
+    follow_per_day = 100,
+    follow_time = 5 * 60 * 60,
+    unfollow_per_day = 90,
+    unfollow_break_min = 20,
+    unfollow_break_max = 40,
+    log_mod = 0,
+    proxy = '',
     # List of list of words, each of which will be used to generate comment
     # For example: "This shot feels wow!"
-    comment_list=[["this", "the", "your"],
-                  ["photo", "picture", "pic", "shot", "snapshot"],
-                  ["is", "looks", "feels", "is really"],
-                  ["great", "super", "good", "very good", "good", "wow",
-                   "WOW", "cool", "GREAT","magnificent", "magical",
-                   "very cool", "stylish", "beautiful", "so beautiful",
-                   "so stylish", "so professional", "lovely",
-                   "so lovely", "very lovely", "glorious","so glorious",
-                   "very glorious", "adorable", "excellent", "amazing"],
+    comment_list=[["essa", "a", "sua"],
+                  ["foto", "imagem"],
+                  ["é", "é realmente"],
+                  ["linda", "fantástica"],
                   [".", "..", "...", "!", "!!", "!!!"]],
     # Use unwanted_username_list to block usernames containing a string
     ## Will do partial matches; i.e. 'mozart' will block 'legend_mozart'
     ### 'free_followers' will be blocked because it contains 'free'
-    unwanted_username_list=[
-        'second', 'stuff', 'art', 'project', 'love', 'life', 'food', 'blog',
-        'free', 'keren', 'photo', 'graphy', 'indo', 'travel', 'art', 'shop',
-        'store', 'sex', 'toko', 'jual', 'online', 'murah', 'jam', 'kaos',
-        'case', 'baju', 'fashion', 'corp', 'tas', 'butik', 'grosir', 'karpet',
-        'sosis', 'salon', 'skin', 'care', 'cloth', 'tech', 'rental', 'kamera',
-        'beauty', 'express', 'kredit', 'collection', 'impor', 'preloved',
-        'follow', 'follower', 'gain', '.id', '_id', 'bags'
-    ],
-    unfollow_whitelist=['example_user_1', 'example_user_2'])
-while True:
+    unwanted_username_list = confs["unwantedUsernameList"],
+    unfollow_whitelist = confs["unfollowWhitelist"])
 
+while True:
     #print("# MODE 0 = ORIGINAL MODE BY LEVPASHA")
     #print("## MODE 1 = MODIFIED MODE BY KEMONG")
     #print("### MODE 2 = ORIGINAL MODE + UNFOLLOW WHO DON'T FOLLOW BACK")
@@ -66,11 +71,11 @@ while True:
     # DON'T USE MODE 5 FOR A LONG PERIOD. YOU RISK YOUR ACCOUNT FROM GETTING BANNED
     ## USE MODE 5 IN BURST MODE, USE IT TO UNFOLLOW PEOPLE AS MANY AS YOU WANT IN SHORT TIME PERIOD
 
-    mode = 0
+    mode = 2
 
-    #print("You choose mode : %i" %(mode))
-    #print("CTRL + C to cancel this operation or wait 30 seconds to start")
-    #time.sleep(30)
+    print("Running mode : %i" %(mode))
+    print("CTRL + C to cancel this operation or wait 10 seconds to start")
+    time.sleep(10)
 
     if mode == 0:
         bot.new_auto_mod()
